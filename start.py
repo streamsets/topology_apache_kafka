046129a9a880908028d943bd43b62082b1625709
Jarek Jarcec Cecho <jarcec@users.noreply.github.com>
2017-11-13 09:11:43 -0800

    Add support for cluster mode

--- start.py
+++ start.py
@@ -11,6 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import json
 import logging
 import tempfile
 import yaml
@@ -20,11 +21,41 @@ from clusterdock.models import Cluster, Node
 from clusterdock.utils import wait_for_condition
 
 DEFAULT_NAMESPACE = 'clusterdock'
+ZOOKEEPER_PORT = 2181
+BROKER_PORT = 9092
 
 logger = logging.getLogger('clusterdock.{}'.format(__name__))
 
+def success(time):
+    logger.info('Conditions satisfied after %s seconds.', time)
+
+
+def failure(timeout):
+    raise TimeoutError('Timed out after {} seconds waiting.'.format(timeout))
+
+
+# Validate that Zookeeper is up and running by connecting using shell
+def validate_zookeeper(node, quiet):
+    return node.execute('/kafka/bin/zookeeper-shell.sh localhost:2181 ls /', quiet=quiet).exit_code == 0
+
+
+# Validate that Kafka is up by checking that all brokers are registered in zookeeper
+def validate_kafka(node, broker_count, quiet):
+    command = node.execute('/kafka/bin/zookeeper-shell.sh localhost:2181 ls /brokers/ids | tail -n 1', quiet=quiet)
+
+    if command.exit_code != 0:
+        return False
+
+    nodes = command.output
+    if not nodes.startswith('['):
+        return False
+
+    return len(json.loads(nodes)) == broker_count
+
 
 def main(args):
+    quiet = not args.verbose
+
     # Image name
     image = '{}/{}/topology_apache_kafka:kafka-{}-{}'.format(args.registry,
                                                              args.namespace or DEFAULT_NAMESPACE,
@@ -34,16 +65,60 @@ def main(args):
     # Nodes in the Kafka cluster
     nodes = [Node(hostname=hostname,
                   group='brokers',
-                  ports=[2181, 9092],
+                  ports=[ZOOKEEPER_PORT, BROKER_PORT],
                   image=image)
              for hostname in args.brokers]
 
     cluster = Cluster(*nodes)
     cluster.start(args.network, pull_images=args.always_pull)
 
-    # TODO: Add support for cluster mode (e.g. all nodes are part of the same cluster). Today we only
-    # start each node independently so they will all end up independent one-node clusters.
+    # Create distributed zookeeper configuration
+    zookeeper_config = ('tickTime=2000\n',
+                        'dataDir=/zookeeper\n',
+                        'clientPort=2181\n',
+                        'initLimit=5\n',
+                        'syncLimit=2\n')
+    for idx, node in enumerate(cluster):
+        zookeeper_config += 'server.{}={}:2888:3888\n'.format(idx, node.hostname)
 
+    # Start all zookeepers
+    for idx, node in enumerate(cluster):
+        logger.info('Starting Zookeeper on node {}'.format(node.hostname))
+        node.execute('mkdir -p /zookeeper')
+        node.put_file('/zookeeper/myid', str(idx))
+        node.put_file('/zookeeper.properties', zookeeper_config)
+        node.execute('/start_zookeeper &', detach=True)
+
+    # Validate that Zookeepr is alive from each node
     for node in cluster:
-        node.execute('/kafka/bin/zookeeper-server-start.sh /kafka/config/zookeeper.properties &', detach=True)
-        node.execute('/kafka/bin/kafka-server-start.sh /kafka/config/server.properties &', detach=True)
+        logger.info('Validating Zookeeper on node %s', node.hostname)
+        wait_for_condition(condition=validate_zookeeper,
+                           condition_args=[node, quiet],
+                           time_between_checks=3,
+                           timeout=60,
+                           success=success,
+                           failure=failure)
+
+    # Start all brokers
+    for idx, node in enumerate(cluster):
+        logger.info('Starting Kafka on node {}'.format(node.hostname))
+
+        kafka_config = node.get_file('/kafka/config/server.properties')
+        kafka_config = kafka_config.replace('broker.id=0', 'broker.id={}'.format(idx))
+        node.put_file('/kafka.properties', kafka_config)
+
+        node.execute('/start_kafka &', detach=True)
+
+    # Verify that all Kafka brokers up
+    logger.info('Waiting on all brokers to register in zookeeper')
+    wait_for_condition(condition=validate_kafka,
+                       condition_args=[nodes[0], len(nodes), quiet],
+                       time_between_checks=3,
+                       timeout=60,
+                       success=success,
+                       failure=failure)
+
+    # Automatically create topics
+    for topic in args.topics.split(','):
+        logger.info('Creating topic %s', topic)
+        nodes[0].execute('/create_topic {}'.format(topic), quiet=quiet)
